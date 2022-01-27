2ad0a2859ee983a410715eea02c2252bd1393c08
HandsomeDingor <jay807541931@gmail.com>
2022-01-27 03:28:34 -0500

    add 5days weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,45 @@
 var APIkey = "350d6ec5fe090d500d1f7a39a9d83803";
 var cityname = "new york"
 
+
+var currentCityEl = document.getElementById("currentCity")
+var currentDayEl = document.getElementById("currentDay")
+var currentWeatherIconEl = document.getElementById("currentWeatherIcon")
 var nowTempEl = document.getElementById("nowTemp")
 var nowWindEl = document.getElementById("nowWind")
 var nowHumidityEl = document.getElementById("nowHumidity")
 var nowIndexEl = document.getElementById("nowIndex")
 
+
+var day1El =  document.getElementById("day1")
+var day1ImgEl = document.getElementById("day1Img")
+var day1TempEl =  document.getElementById("day1Temp")
+var day1WindEL =  document.getElementById("day1Wind")
+var day1HumidityEl =  document.getElementById("day1Humidity")
+
+var day2El =  document.getElementById("day2")
+var day2ImgEl = document.getElementById("day2Img")
+var day2TempEl =  document.getElementById("day2Temp")
+var day2WindEL =  document.getElementById("day2Wind")
+var day2HumidityEl =  document.getElementById("day2Humidity")
+
+var day3El =  document.getElementById("day3")
+var day3ImgEl = document.getElementById("day3Img")
+var day3TempEl =  document.getElementById("day3Temp")
+var day3WindEL =  document.getElementById("day3Wind")
+var day3HumidityEl =  document.getElementById("day3Humidity")
+
+var day4El =  document.getElementById("day4")
+var day4ImgEl = document.getElementById("day4Img")
+var day4TempEl =  document.getElementById("day4Temp")
+var day4WindEL =  document.getElementById("day4Wind")
+var day4HumidityEl =  document.getElementById("day4Humidity")
+
+var day5El =  document.getElementById("day5")
+var day5ImgEl = document.getElementById("day5Img")
+var day5TempEl =  document.getElementById("day5Temp")
+var day5WindEL =  document.getElementById("day5Wind")
+var day5HumidityEl =  document.getElementById("day5Humidity")
 
 
 function getNowCity() {
@@ -18,16 +52,36 @@ function getNowCity() {
 
         }).then(function (data) {
             console.log(data)
+            currentCityEl.textContent = data.name
+            currentDayEl.textContent = moment().format('YYYY-MM-D' );
+            currentWeatherIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
             nowTempEl.textContent = "Temp: " + data.main.temp + " °F"
             nowWindEl.textContent = "wind: " + data.wind.speed + " MPH"
             nowHumidityEl.textContent = "Humidity: " + data.main.humidity + " %"
-            nowIndexEl.textContent = "wind: " + data.wind.speed + " MPH"
 
+            var uvUrl = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + data.coord.lat + "&lon=" + data.coord.lon
+            fetch(uvUrl)
+                .then(function (response) {
+
+                    return response.json()
+
+                }).then(function (data) {
+
+                    console.log(uvUrl)
+                    nowIndexEl.textContent = "UV Index: " + data.value
+                }
+                )
 
         })
 };
 
 getNowCity()
+
+
+
+
+
+
 
 
 function get5DaysCity() {
@@ -46,17 +100,47 @@ function get5DaysCity() {
                     ourForecastObject.push({
                         date: forecastData.list[i].dt_txt.split(" ")[0],
                         icon: forecastData.list[i].weather[0].icon,
-                        iconAlt: forecastData.list[i].weather[0].description,
                         temp: forecastData.list[i].main.temp,
+                        wind: forecastData.list[i].wind.speed,
                         humidity: forecastData.list[i].main.humidity,
+                        
                     })
                 }
 
             } console.log(ourForecastObject)
-            // nowTempEl.textContent = "Temp: " + data.main.temp + " °F"
-            // nowWindEl.textContent = "wind: " + data.wind.speed + " MPH"
-            // nowHumidityEl.textContent = "Humidity: " + data.main.humidity + " %"
-            // nowIndexEl.textContent = "wind: " + data.wind.speed + " MPH"
+
+            day1El.textContent = ourForecastObject[0].date
+            day1ImgEl.setAttribute("src", "http://openweathermap.org/img/wn/" + ourForecastObject[0].icon + "@2x.png")
+            day1TempEl.textContent = "Temp: " + ourForecastObject[0].temp
+            day1WindEL.textContent = "Wind: " + ourForecastObject[0].wind
+            day1HumidityEl.textContent = "Humidity: " + ourForecastObject[0].humidity
+
+            day2El.textContent = ourForecastObject[1].date
+            day2ImgEl.setAttribute("src", "http://openweathermap.org/img/wn/" + ourForecastObject[1].icon + "@2x.png")
+            day2TempEl.textContent = "Temp: " + ourForecastObject[1].temp
+            day2WindEL.textContent = "Wind: " + ourForecastObject[1].wind
+            day2HumidityEl.textContent = "Humidity: " + ourForecastObject[1].humidity
+
+            day3El.textContent = ourForecastObject[2].date
+            day3ImgEl.setAttribute("src", "http://openweathermap.org/img/wn/" + ourForecastObject[2].icon + "@2x.png")
+            day3TempEl.textContent = "Temp: " + ourForecastObject[2].temp
+            day3WindEL.textContent = "Wind: " + ourForecastObject[2].wind
+            day3HumidityEl.textContent = "Humidity: " + ourForecastObject[2].humidity
+
+            day4El.textContent = ourForecastObject[3].date
+            day4ImgEl.setAttribute("src", "http://openweathermap.org/img/wn/" + ourForecastObject[3].icon + "@2x.png")
+            day4TempEl.textContent = "Temp: " + ourForecastObject[3].temp
+            day4WindEL.textContent = "Wind: " + ourForecastObject[3].wind
+            day4HumidityEl.textContent = "Humidity: " + ourForecastObject[3].humidity
+
+            day5El.textContent = ourForecastObject[4].date
+            day5ImgEl.setAttribute("src", "http://openweathermap.org/img/wn/" + ourForecastObject[4].icon + "@2x.png")
+            day5TempEl.textContent = "Temp: " + ourForecastObject[4].temp
+            day5WindEL.textContent = "Wind: " + ourForecastObject[4].wind
+            day5HumidityEl.textContent = "Humidity: " + ourForecastObject[4].humidity
+
+
+
         })
 };
 
